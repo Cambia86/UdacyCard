c114d96bf6a859f768b4d81e7dd4ac7265c6792d
cambia86 <alessandro.cambiaghi@gmail.com>
2017-11-13 18:00:10 +0100

    add documentation

--- utils/api.js
+++ utils/api.js
@@ -1,12 +1,18 @@
 import { AsyncStorage } from 'react-native'
 import { formatDeckResults, formatDeckByIdResults, DECKAPP_STORAGE_KEY } from './_deckapp'
 
-
+/**
+* @description Fetch all deck in async storage and return a formatted result
+*/
 export function fetchDeckResults() {
   return AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
     .then(formatDeckResults)
 }
 
+/**
+* @description Create a deck
+* @param {object} deck - info of the deck
+*/
 export function submitDeck(deck) {
   AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
     .then((result) => {
@@ -15,63 +21,33 @@ export function submitDeck(deck) {
       if (data && !data.decks) {
         data.decks = []
       }
-      console.log("api newCard: DECKAPP_STORAGE_KEY get: " + JSON.stringify(result))
       data.decks.push(deck)
       AsyncStorage.setItem(DECKAPP_STORAGE_KEY, JSON.stringify(data))
-      // AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
-      // .then((result) => {
-      //   console.log("api newCard: DECKAPP_STORAGE_KEY get: " + JSON.stringify(result))
-      // })
+
     }
     )
 }
 
+/**
+* @description Add card to a specified deck
+* @param {objcet} obj - The object rapresented bu a card (question - answer)
+*/
 export function addCard(obj) {
   AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
     .then((result) => {
       const data = JSON.parse(result)
       if (data && data.decks) {
-       // data.decks[obj.deckId].questions.push(obj.quest)
-       data.decks.map((item)=>{
-          if(item.id==obj.deckId){
+        data.decks.map((item) => {
+          if (item.id == obj.deckId) {
             item.questions.push(obj.quest)
           }
-       })
+        })
         AsyncStorage.setItem(DECKAPP_STORAGE_KEY, JSON.stringify(data))
       }
-        AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
-      .then((result) => {
-        console.log("api newCard: DECKAPP_STORAGE_KEY get: " + JSON.stringify(result))
-      })
+      // AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
+      //   .then((result) => {
+      //   })
     }
     )
 
-  // console.log("api newCard:" + JSON.stringify(DECKAPP_STORAGE_KEY))
-  // return AsyncStorage.mergeItem(DECKAPP_STORAGE_KEY, JSON.stringify({
-  //   [deck.id]: deck
-  // }))
-
-
-  // return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
-  //   .then((results) => {
-  //     const data = JSON.parse(results)
-  //     data[key] = undefined
-  //     delete data[key]
-  //     AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
-  //   })
 }
-
-
-//   // return AsyncStorage.mergeItem(DECKAPP_STORAGE_KEY, JSON.stringify({
-//   //   [deck.id]: deck
-//   // }))
-// }
-
-// export function addCard(card){
-//   return AsyncStorage.getItem(DECKAPP_STORAGE_KEY)
-//   .then((results) => {
-//     const data = JSON.parse(results)
-//     data[deck.id] = deck
-//     AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
-//   })
-// }
